fa5a3aeca781133d320ce607612016ec7bc3e394
kkkkendei <authoryu0607@gmail.com>
2025-02-23 22:11:24 +0800

    feat: 配置两个库表Dao

---
+++ group-buy-market-infrastructure/src/main/java/com/wuzeyu/infrastructure/dao/IGroupBuyActivityDao.java
@@ -0,0 +1,24 @@
+package com.wuzeyu.infrastructure.dao;
+
+import com.wuzeyu.infrastructure.dao.po.GroupBuyActivity;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+/**
+ * @author wuzeyu
+ * @description 拼团活动Dao
+ * @github github.com/kkkkendei
+ */
+@Mapper
+public interface IGroupBuyActivityDao {
+
+    List<GroupBuyActivity> queryGroupBuyActivityList();
+
+    GroupBuyActivity queryValidGroupBuyActivity(GroupBuyActivity groupBuyActivityReq);
+
+    GroupBuyActivity queryValidGroupBuyActivityId(Long activityId);
+
+    GroupBuyActivity queryGroupBuyActivityByActivityId(Long activityId);
+
+}
---
+++ group-buy-market-infrastructure/src/main/java/com/wuzeyu/infrastructure/dao/IGroupBuyDiscountDao.java
@@ -0,0 +1,20 @@
+package com.wuzeyu.infrastructure.dao;
+
+import com.wuzeyu.infrastructure.dao.po.GroupBuyDiscount;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+/**
+ * @author wuzeyu
+ * @description 折扣配置Dao
+ * @github github.com/kkkkendei
+ */
+@Mapper
+public interface IGroupBuyDiscountDao {
+
+    List<GroupBuyDiscount> queryGroupBuyDiscountList();
+
+    GroupBuyDiscount queryGroupBuyActivityDiscountByDiscountId(String discountId);
+
+}
