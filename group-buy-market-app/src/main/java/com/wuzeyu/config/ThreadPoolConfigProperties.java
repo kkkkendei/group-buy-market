d29dda572bbab22e7a629dc888ab3ea0691668c2
kkkkendei <authoryu0607@gmail.com>
2025-05-10 20:09:27 +0800

    chore: packge-info

--- group-buy-market-app/src/main/java/com/wuzeyu/config/ThreadPoolConfigProperties.java
+++ group-buy-market-app/src/main/java/com/wuzeyu/config/ThreadPoolConfigProperties.java
@@ -9,13 +9,17 @@ public class ThreadPoolConfigProperties {
 
     /** 核心线程数 */
     private Integer corePoolSize = 20;
+
     /** 最大线程数 */
     private Integer maxPoolSize = 200;
+
     /** 最大等待时间 */
     private Long keepAliveTime = 10L;
+
     /** 最大队列数 */
     private Integer blockQueueSize = 5000;
-    /*
+
+    /**
      * AbortPolicy：丢弃任务并抛出RejectedExecutionException异常。
      * DiscardPolicy：直接丢弃任务，但是不会抛出异常
      * DiscardOldestPolicy：将最早进入队列的任务删除，之后再尝试加入队列的任务被拒绝
@@ -23,43 +27,4 @@ public class ThreadPoolConfigProperties {
      * */
     private String policy = "AbortPolicy";
 
-    public Integer getCorePoolSize() {
-        return corePoolSize;
-    }
-
-    public void setCorePoolSize(Integer corePoolSize) {
-        this.corePoolSize = corePoolSize;
-    }
-
-    public Integer getMaxPoolSize() {
-        return maxPoolSize;
-    }
-
-    public void setMaxPoolSize(Integer maxPoolSize) {
-        this.maxPoolSize = maxPoolSize;
-    }
-
-    public Long getKeepAliveTime() {
-        return keepAliveTime;
-    }
-
-    public void setKeepAliveTime(Long keepAliveTime) {
-        this.keepAliveTime = keepAliveTime;
-    }
-
-    public Integer getBlockQueueSize() {
-        return blockQueueSize;
-    }
-
-    public void setBlockQueueSize(Integer blockQueueSize) {
-        this.blockQueueSize = blockQueueSize;
-    }
-
-    public String getPolicy() {
-        return policy;
-    }
-
-    public void setPolicy(String policy) {
-        this.policy = policy;
-    }
 }
---
+++ group-buy-market-domain/src/main/java/com/wuzeyu/domain/activity/service/trial/factory/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * 通过工厂模式将RootNode设置为唯一入口，保证所有请求都经过相同的初始处理
+ */
+package com.wuzeyu.domain.activity.service.trial.factory;
